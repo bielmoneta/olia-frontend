1ebb83f7cac29c1aeb92dedeaaf4e4dc274c7597
Gabriel Moneta <gabrielpmoneta@gmail.com>
2025-11-12 22:55:05 -0300

    add: rodapé inteligente

--- src/app/componentes/rodape/rodape.ts
+++ src/app/componentes/rodape/rodape.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { Modal } from '../modal/modal';
 
 @Component({
   selector: 'app-rodape',
   standalone: true,
-  imports: [Modal],
+  imports: [CommonModule, RouterModule, Modal],
   templateUrl: './rodape.html',
   styleUrl: './rodape.css',
 })
 export class Rodape {
-  mostrarModal = false;
+  private router = inject(Router);
 
+  mostrarModal = false;
   abrirModal() {
     this.mostrarModal = !this.mostrarModal;
+  }
+
+  isPaginaInicial(): boolean {
+    return this.router.url === '/'; // retorna true apenas se a URL for exatamente a barra "/" --> volta para a página inicial
   }
 }
