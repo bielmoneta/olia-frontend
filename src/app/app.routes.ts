8db38214d105a8e39b84907d3b5df9c8479f0f13
Gabriel Moneta <gabrielpmoneta@gmail.com>
2025-11-12 22:50:41 -0300

    add: novas rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './componentes/inicio/inicio';
+import { LoginUsuario } from './componentes/login-usuario/login-usuario';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: Inicio},
+  {path: 'login-usuario', component: LoginUsuario},
+];
