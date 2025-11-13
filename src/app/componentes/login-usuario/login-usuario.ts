e40211ba0afaf582c0021174523bccebe808ab04
Gabriel Moneta <gabrielpmoneta@gmail.com>
2025-11-12 22:54:43 -0300

    add: novo componente de login de usuário

---
+++ src/app/componentes/login-usuario/login-usuario.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login-usuario',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './login-usuario.html',
+  styleUrl: './login-usuario.css',
+})
+export class LoginUsuario {
+
+}
