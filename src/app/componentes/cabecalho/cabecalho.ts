a5b417a21b4dcb50807c906694ccb8b613f73f97
Gabriel Moneta <gabrielpmoneta@gmail.com>
2025-11-12 22:53:31 -0300

    add: cabeçalho inteligente, nova estilização e imports

--- src/app/componentes/cabecalho/cabecalho.ts
+++ src/app/componentes/cabecalho/cabecalho.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cabecalho',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule, CommonModule],
   templateUrl: './cabecalho.html',
   styleUrl: './cabecalho.css',
 })
 export class Cabecalho {
+  private router = inject(Router); //injeta o roteador para ler a url das páginas
 
+  isPaginaLogin(): boolean {
+    return this.router.url === '/login-usuario'
+  }
 }
