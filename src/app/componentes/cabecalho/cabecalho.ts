3ef7dd993cfca41fd04adbddde55577f8937bf53
Gabriel Moneta <gabrielpmoneta@gmail.com>
2025-11-15 17:08:29 -0300

    add: nova lógica para a seta de voltar no cabeçalho

--- src/app/componentes/cabecalho/cabecalho.ts
+++ src/app/componentes/cabecalho/cabecalho.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
 
 @Component({
   selector: 'app-cabecalho',
@@ -11,8 +11,33 @@ import { CommonModule } from '@angular/common';
 })
 export class Cabecalho {
   private router = inject(Router); //injeta o roteador para ler a url das páginas
+  private location = inject(Location); //injeta o location para manipular o histórico de navegação
 
   isPaginaLogin(): boolean {
-    return this.router.url === '/login-usuario'
+    const url = this.router.url;
+    return url.includes('/login') || url.includes('/cadastro');
   }
+
+  voltar() {
+    const urlAtual = this.router.url;
+
+  // Se estiver na página de cadastro de usuário
+    if (urlAtual === '/cadastro-usuario') {
+    // ele volta para o login de usuário.
+    this.router.navigate(['/login-usuario']);
+    }
+
+  //  Se estiver na página de login de usuário
+    else if (urlAtual === '/login-usuario') {
+    // ele volta para o inicio
+    this.router.navigate(['/']);
+    }
+
+    // TODO: adicionar regras para escola e governo)
+
+      // Para qualquer outro caso
+      else {
+    this.location.back();
+      }
+    }
 }
