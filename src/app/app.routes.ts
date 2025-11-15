b415f5a1890af3dcb93be0c4ed020454e6303701
Gabriel Moneta <gabrielpmoneta@gmail.com>
2025-11-15 19:31:56 -0300

    add: novos path e imports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { Inicio } from './componentes/inicio/inicio';
 import { LoginUsuario } from './componentes/login-usuario/login-usuario';
+import { CadastroUsuario } from './componentes/cadastro-usuario/cadastro-usuario';
+import { MapaUsuario } from './componentes/mapa-usuario/mapa-usuario';
 
 export const routes: Routes = [
-  {path: '', component: Inicio},
-  {path: 'login-usuario', component: LoginUsuario},
+  {path: '', component: Inicio  },
+  {path: 'login-usuario', component: LoginUsuario },
+  { path: 'cadastro-usuario', component: CadastroUsuario },
+  { path: 'mapa-usuario', component: MapaUsuario }
 ];
