84ea3a7d94695888d16ddda474cda05a28712864
Gabriel Moneta <gabrielpmoneta@gmail.com>
2025-11-15 19:30:38 -0300

    add: novos imports e nova função para o cabeçalho na página inicial e de dentro do app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,31 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, inject} from '@angular/core';
+import { RouterOutlet, Router, RouterLink, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { Cabecalho } from "./componentes/cabecalho/cabecalho";
-import { Inicio } from "./componentes/inicio/inicio";
-import { Rodape } from "./componentes/rodape/rodape";
-import { Modal } from "./componentes/modal/modal";
+import { Rodape } from './componentes/rodape/rodape';
+import { RodapeUsuario } from './componentes/rodape-usuario/rodape-usuario';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Cabecalho, Inicio, Rodape],
+  imports: [RouterOutlet, Cabecalho, RodapeUsuario, Rodape, CommonModule, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('OLIA-Frontend');
+  private router = inject(Router);
+
+  // retorna true se for o inicio
+  isPaginaInicial(): boolean {
+    return this.router.url === '/';
+  }
+
+  // Retorna true se for uma página do app (mapa, perfil, historico etc.)
+  isPaginaApp(): boolean {
+    const url = this.router.url;
+    return url.includes('/mapa-usuario') || url.includes('/perfil-usuario') || url.includes('/historico-usuario') || url.includes('/recompensa-usuario');
+  }
+
+  isLogado(): boolean {
+    return sessionStorage.getItem('usuarioLogado') === 'true';
+  }
 }
