0c6c3bd8cc62b33294b3b38861c41eef088e30ad
Gabriel Moneta <gabrielpmoneta@gmail.com>
2025-11-23 23:41:13 -0300

    add: função de listar e buscar coordenadas da escola

--- src/app/services/escola.service.ts
+++ src/app/services/escola.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { DadosCadastroEscola } from '../modelos-java/dados-escola';
 
 @Injectable({ providedIn: 'root' })
@@ -15,5 +15,19 @@ export class EscolaService {
 
   fazerLogin(dados: {email: string, senha: string}): Observable<any> {
     return this.http.post(this.apiUrlLoginEscola, dados);
+  }
+
+  listar() {
+    // O Java retorna um objeto Page { content: [...] }
+    // Usamos o pipe(map) para pegar só a lista que está dentro do 'content'
+    return this.http.get<any>(this.apiUrlEscola).pipe(
+      map(resposta => resposta.content)
+    );
+  }
+
+  // Método auxiliar para transformar Endereço em Coordenadas (Geocoding)
+  buscarCoordenadas(endereco: string): Observable<any> {
+    const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(endereco)}`;
+    return this.http.get(url);
   }
 }
